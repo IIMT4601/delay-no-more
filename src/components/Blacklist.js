1c0225f6a9f60cdfcc62be343fc320a3fbb580d4
emlylam <emly@connect.hku.hk>
2018-03-09 15:14:02 +0800

    blacklist first commit

--- src/components/Blacklist.js
+++ src/components/Blacklist.js
@@ -3,17 +3,32 @@ import React, { Component } from 'react';
 class Blacklist extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      blacklist: [
+        "www.google.com",
+        "www.gmail.com"
+      ]
+    }
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+
+
+  }
 
   componentWillUnmount() {}
+
 
   render() {
     return (
       <div>
         <h1>My Blacklist:</h1>
+        <input ref="myInput" type="text"/>
+        <ul>
+          {this.state.blacklist.map(site => (
+            <li>{site}</li>
+          ))}
+        </ul>
       </div>
     );
   }
