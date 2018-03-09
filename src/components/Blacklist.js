f6ab1c170a81d49063a7e837a994e4e698222c66
emlylam <emly@connect.hku.hk>
2018-03-09 16:33:34 +0800

    delete site from blacklist functionality

--- src/components/Blacklist.js
+++ src/components/Blacklist.js
@@ -11,9 +11,7 @@ class Blacklist extends Component {
     }
   }
 
-  componentDidMount() {
-
-  }
+  componentDidMount() {}
 
   componentWillUnmount() {}
 
@@ -24,7 +22,15 @@ class Blacklist extends Component {
         blacklist: [e.target.value, ...this.state.blacklist]
       });
       e.target.value = ""; //clear text area after submit
+      console.log("handleKeyPress:", this.state);
     }
+  }
+
+  handleDelete = (i) => {
+    this.setState({
+      blacklist: [...this.state.blacklist.slice(0, i), ...this.state.blacklist.slice(i + 1)]
+    });
+    console.log("handleDelete:", this.state);
   }
 
   render() {
@@ -34,7 +40,10 @@ class Blacklist extends Component {
         <input placeholder="Enter to add a site to Blacklist..." onKeyPress={e => this.handleKeyPress(e)}/>
         <ul>
           {this.state.blacklist.map((site, index) => (
-            <li key={index}>{site}</li>
+            <li key={index}>
+              {site}
+              <button onClick={() => this.handleDelete(index)}>Remove Site</button>
+            </li>
           ))}
         </ul>
       </div>
