eb2e1d302518391ed1a433e6e07761e415dae6bf
Ian Liu <ian1654@gmail.com>
2018-03-18 01:39:05 +0800

    Basic pie chart styling

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -28,6 +28,19 @@ class Analytics extends Component {
 
   componentWillUnmount() {}
 
+  millisecToTime = duration => {
+    let milliseconds = parseInt((duration%1000)/100);
+    let seconds = parseInt((duration/1000)%60);
+    let minutes = parseInt((duration/(1000*60))%60);
+    let hours = parseInt((duration/(1000*60*60))%24);
+  
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+    return hours + "h " + minutes + "m " + seconds + "s";
+  }
+
   render() {
     const d = new Date();
     const todaysDate = d.getDate() + "-" + (d.getMonth()+1) + "-" + d.getFullYear();
@@ -38,7 +51,8 @@ class Analytics extends Component {
         pieData.push({
           id: v.siteHost, 
           label: v.siteHost, 
-          value: v.duration
+          value: v.duration,
+          time: this.millisecToTime(v.duration)
         });
       });      
     }
@@ -86,6 +100,8 @@ class Analytics extends Component {
                 "symbolShape": "circle"
               }
             ]}
+            radialLabel={d => `${d.id} (${d.time})`}
+            enableSlicesLabels={false}
           />          
         </div>
       </div>
