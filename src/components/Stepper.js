66c91dd1e07c0e047821e3138582d450ba7b0489
emlylam <emly@connect.hku.hk>
2018-04-10 10:52:00 +0800

    imported horizontal Stepper from material-ui

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -1,29 +1,110 @@
 import React, { Component } from 'react';
+import {
+  Step,
+  Stepper,
+  StepLabel,
+} from 'material-ui/Stepper';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 
-class Stepper extends Component {
+class Setup extends Component {
   constructor() {
     super();
     this.state = {
+      finished: false,
+      stepIndex: 0,
     }
   }
 
-  componentDidMount() {
-
-  }
+  componentDidMount() {}
 
   componentWillUnmount() {}
+
+  handleNext = () => {
+    const {stepIndex} = this.state;
+    this.setState({
+      stepIndex: stepIndex + 1,
+      finished: stepIndex >= 2,
+    });
+  };
+
+  handlePrev = () => {
+    const {stepIndex} = this.state;
+    if (stepIndex > 0) {
+      this.setState({stepIndex: stepIndex - 1});
+    }
+  };
+
+  getStepContent(stepIndex) {
+    switch (stepIndex) {
+      case 0:
+        return 'Define your own list of websites to blacklist...';
+      case 1:
+        return 'This is step 2...';
+      case 2:
+        return 'This is step 3...';
+      default:
+        return 'You\'re a long way from home sonny jim!';
+    }
+  }
+
+
 
 
   render() {
 
+    const {finished, stepIndex} = this.state;
+    const contentStyle = {margin: '0 16px'};
 
 
     return (
-      <div>
-       <h1>Stepper</h1>
+      <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
+        <Stepper activeStep={stepIndex}>
+          <Step>
+            <StepLabel>Create your Blacklist</StepLabel>
+          </Step>
+          <Step>
+            <StepLabel>Step 2...</StepLabel>
+          </Step>
+          <Step>
+            <StepLabel>Step 3...</StepLabel>
+          </Step>
+        </Stepper>
+        <div style={contentStyle}>
+          {finished ? (
+            <p>
+              <a
+                href="#"
+                onClick={(event) => {
+                  event.preventDefault();
+                  this.setState({stepIndex: 0, finished: false});
+                }}
+              >
+                Click here
+              </a> to reset the example.
+            </p>
+          ) : (
+            <div>
+              <p>{this.getStepContent(stepIndex)}</p>
+              <div style={{marginTop: 12}}>
+                <FlatButton
+                  label="Back"
+                  disabled={stepIndex === 0}
+                  onClick={this.handlePrev}
+                  style={{marginRight: 12}}
+                />
+                <RaisedButton
+                  label={stepIndex === 2 ? 'Finish' : 'Next'}
+                  primary={true}
+                  onClick={this.handleNext}
+                />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
 }
 
-export default Stepper;
+export default Setup;
