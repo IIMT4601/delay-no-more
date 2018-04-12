800f161315416d9d62879918d2e3a651db38c759
emlylam <emly@connect.hku.hk>
2018-04-12 10:56:55 +0800

    added blacklist component into first step of stepper

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -6,11 +6,15 @@ import {
 } from 'material-ui/Stepper';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
+import ExpandTransition from 'material-ui/internal/ExpandTransition';
+import TextField from 'material-ui/TextField';
+import Blacklist from './Blacklist';
 
 class Setup extends Component {
   constructor() {
     super();
     this.state = {
+      loading: false,
       finished: false,
       stepIndex: 0,
     }
@@ -20,88 +24,135 @@ class Setup extends Component {
 
   componentWillUnmount() {}
 
+  dummyAsync = (cb) => {
+    this.setState({loading: true}, () => {
+      this.asyncTimer = setTimeout(cb, 500);
+    });
+  };
+
+
   handleNext = () => {
     const {stepIndex} = this.state;
-    this.setState({
-      stepIndex: stepIndex + 1,
-      finished: stepIndex >= 2,
-    });
+    if (!this.state.loading) {
+      this.dummyAsync(() => this.setState({
+        loading: false,
+        stepIndex: stepIndex + 1,
+        finished: stepIndex >= 2,
+      }));
+    }
   };
 
   handlePrev = () => {
     const {stepIndex} = this.state;
-    if (stepIndex > 0) {
-      this.setState({stepIndex: stepIndex - 1});
+    if (!this.state.loading) {
+      this.dummyAsync(() => this.setState({
+        loading: false,
+        stepIndex: stepIndex - 1,
+      }));
     }
   };
+
 
   getStepContent(stepIndex) {
     switch (stepIndex) {
       case 0:
-        return 'Define your own list of websites to blacklist...';
+        return (
+          <div>
+            <p>To setup your blacklist, please enter websites you want to blacklist below.</p>
+            <Blacklist>
+            </Blacklist>
+          </div>
+
+
+        );
       case 1:
-        return 'This is step 2...';
+        return (
+          <div>
+            <TextField style={{marginTop: 0}} floatingLabelText="Ad group name" />
+            <p>
+              Ad group status is different than the statuses for campaigns, ads, and keywords, though the
+              statuses can affect each other. Ad groups are contained within a campaign, and each campaign can
+              have one or more ad groups. Within each ad group are ads, keywords, and bids.
+            </p>
+            <p>Something something whatever cool</p>
+          </div>
+        );
       case 2:
-        return 'This is step 3...';
+        return (
+          <p>
+            Try out different ad text to see what brings in the most customers, and learn how to
+            enhance your ads using features like ad extensions. If you run into any problems with your
+            ads, find out how to tell if they're running and how to resolve approval issues.
+          </p>
+        );
       default:
         return 'You\'re a long way from home sonny jim!';
     }
   }
 
+  renderContent() {
+    const {finished, stepIndex} = this.state;
+    const contentStyle = {margin: '0 16px', overflow: 'hidden'};
 
+    if (finished) {
+      return (
+        <div style={contentStyle}>
+          <p>
+            <a
+              href="#"
+              onClick={(event) => {
+                event.preventDefault();
+                this.setState({stepIndex: 0, finished: false});
+              }}
+            >
+              Click here
+            </a> to reset the example.
+          </p>
+        </div>
+      );
+    }
+
+    return (
+      <div style={contentStyle}>
+        <div>{this.getStepContent(stepIndex)}</div>
+        <div style={{marginTop: 24, marginBottom: 12}}>
+          <FlatButton
+            label="Back"
+            disabled={stepIndex === 0}
+            onClick={this.handlePrev}
+            style={{marginRight: 12}}
+          />
+          <RaisedButton
+            label={stepIndex === 2 ? 'Finish' : 'Next'}
+            primary={true}
+            onClick={this.handleNext}
+          />
+        </div>
+      </div>
+    );
+  }
 
 
   render() {
 
-    const {finished, stepIndex} = this.state;
-    const contentStyle = {margin: '0 16px'};
-
+    const {loading, stepIndex} = this.state;
 
     return (
       <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
         <Stepper activeStep={stepIndex}>
           <Step>
-            <StepLabel>Create your Blacklist</StepLabel>
+            <StepLabel>Create Blacklist</StepLabel>
           </Step>
           <Step>
-            <StepLabel>Step 2...</StepLabel>
+            <StepLabel>Create an ad group</StepLabel>
           </Step>
           <Step>
-            <StepLabel>Step 3...</StepLabel>
+            <StepLabel>Create an ad</StepLabel>
           </Step>
         </Stepper>
-        <div style={contentStyle}>
-          {finished ? (
-            <p>
-              <a
-                href="#"
-                onClick={(event) => {
-                  event.preventDefault();
-                  this.setState({stepIndex: 0, finished: false});
-                }}
-              >
-                Click here
-              </a> to reset the example.
-            </p>
-          ) : (
-            <div>
-              <p>{this.getStepContent(stepIndex)}</p>
-              <div style={{marginTop: 12}}>
-                <FlatButton
-                  label="Back"
-                  disabled={stepIndex === 0}
-                  onClick={this.handlePrev}
-                  style={{marginRight: 12}}
-                />
-                <RaisedButton
-                  label={stepIndex === 2 ? 'Finish' : 'Next'}
-                  primary={true}
-                  onClick={this.handleNext}
-                />
-              </div>
-            </div>
-          )}
-        </div>
+        <ExpandTransition loading={loading} open={true}>
+          {this.renderContent()}
+        </ExpandTransition>
       </div>
     );
   }
