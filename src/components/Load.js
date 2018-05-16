9d9c65df68b2cd3c1da0e6aacec8373203b330f1
Ian Liu <ian1654@gmail.com>
2018-05-16 17:38:32 +0800

    Fix indent

--- src/components/Load.js
+++ src/components/Load.js
@@ -5,30 +5,30 @@ import Lottie from 'react-lottie';
 import * as load_animation from '../animations/preloader_02.json';
 
 const defaultOptions = {
-    loop: true,
-    autoplay: true, 
-    animationData: load_animation,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid',
-    }
-  };
+  loop: true,
+  autoplay: true, 
+  animationData: load_animation,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid',
+  }
+};
 
 class Load extends Component {
-    constructor(){
-        super();
-    }
-    render(){
-        return (
-            <Lottie options={defaultOptions} 
-                height={330}
-                width={400}
-                isStopped={false}
-                isPaused={false}
-                speed={1.4}
-                direction={1}
-            />
-        );
-    }
+  constructor(){
+    super();
+  }
+  render(){
+    return (
+      <Lottie options={defaultOptions} 
+        height={330}
+        width={400}
+        isStopped={false}
+        isPaused={false}
+        speed={1.4}
+        direction={1}
+      />
+    );
+  }
 } 
 
 export default Load;
